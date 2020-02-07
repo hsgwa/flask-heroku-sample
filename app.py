808d3a95423609d3e6f739116a5a415fc9457f3b
hsgwa <hasegawa.ats@gmail.com>
2020-02-07 20:53:09 +0900

    add: result page and reserve_name

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,39 +10,42 @@ DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
 
-
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100))
+  name = db.Column(db.String(100), unique=True)
+  reserve_name = db.Column(db.String(100))
 
-  def __init__(self, name):
+  def __init__(self, name, reserve_name):
     self.name = name
+    self.reserve_name = reserve_name
 
 
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html', users=User.query.all())
+  user = User.query.all()
+  return render_template('index.html')
 
 @app.route('/result/<username>', methods=['GET'])
 def result(username):
-  users=User.query.all()
-  usernames = [user.name for user in users]
-  registered = username in usernames
+   user = User.query.filter(User.name == username).all()
+   registered = len(user) > 0
+   reserve_name = ''
+   if registered:
+     reserve_name = user[0].reserve_name
+   else:
+    u = User(username, 'ジョイ')
+    db.session.add(u)
+    db.session.commit()
 
-  u = User(username)
-  db.session.add(u)
-  db.session.commit()
-
-  return render_template('index.html', users=User.query.all(), registered=registered)
+   return render_template('result.html', users=User.query.all(), registered=registered, reserve_name=reserve_name)
 
 @app.route('/user', methods=['POST'])
 def user():
   username = request.form['name']
   return redirect(url_for('result', username=username))
-  # # return redirect(url_for('index'))
-  # return redirect(url_for('index'))
 
 if __name__ == '__main__':
+  # db.drop_all(bind=None)
   db.create_all()
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port, debug=True)
