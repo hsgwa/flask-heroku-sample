d9e461d21c0c3128ba576f1c1635471f9f466afd
hsgwa <hasegawa.ats@gmail.com>
2020-02-07 20:59:44 +0900

    add: drop_all for first.

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def user():
   return redirect(url_for('result', username=username))
 
 if __name__ == '__main__':
-  # db.drop_all(bind=None)
+  db.drop_all(bind=None)
   db.create_all()
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port, debug=True)
