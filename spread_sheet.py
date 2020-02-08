9d0854b4b7a7dbb4d4c42116fac485df5336ad85
hsgwa <hasegawa.ats@gmail.com>
2020-02-08 11:37:23 +0900

    add: google spread sheetの読み込みを追加

---
+++ spread_sheet.py
@@ -0,0 +1,21 @@
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
+COLUMN_RANGE = os.environ['COLUMN_RANGE']
+
+headers = {'Authorization' : 'Bearer ' + ACCESS_TOKEN}
+api_uri = 'https://sheets.googleapis.com/v4/spreadsheets/'
+uri = api_uri + SPREADSHEET_ID + '/values/' + COLUMN_RANGE
+
+def get_list():
+    response = requests.get(uri, headers=headers)
+    data = response.json()
+    return data['values']
+
+if __name__ == '__main__':
+    print(get_list())
+
